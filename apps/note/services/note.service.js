c4b6ce731d68fc84f3618d16ad08f2e51aae2f88
Lihi Ben Shimol <lihibs26@gmail.com>
2022-12-28 16:49:38 +0200

    css notes

--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -3,7 +3,7 @@ import { storageService } from '../../../services/storage.service.js'
 import { asyncStorageService } from '../../../services/async-storage.service.js'
 
 
-const STORAGE_KEY = 'notesDB'
+const NOTE_KEY = 'notesDB'
 _createNotes()
 
 export const noteService = {
@@ -13,51 +13,61 @@ export const noteService = {
 //     save,
 }
 
-function _createNotes() {
-
-    let notes = storageService.loadFromStorage(STORAGE_KEY)
-
-    if (!notes || !notes.length) {
-
-        notes = [
-            {
-                id: "n101",
-                type: "note-txt",
-                isPinned: true,
-                info: {
-                    txt: "Fullstack Me Baby!"
-                }
-            },
-            {
-                id: "n103",
-                type: "note-todos",
-                info: {
-                    lable: "Get my stuff together", todos: [
-                        { txt: "Driving liscence", doneAt: null },
-                        { txt: "Coding power", doneAt: 187111111 }]
-                }
-            },
-            {
-                id: "n102",
-                type: "note-img",
-                info: {
-                    url: "../../assets/img/fiat.jpg",
-                    title: "Bobi and Me"
-                },
-                style: {
-                    backgroundColor: "#00d"
-                }
-            }
-        ]
-    }
-
-    storageService.saveToStorage(STORAGE_KEY, notes)
-}
 
 function query() {
-    return asyncStorageService.query(STORAGE_KEY)
-        .then((notes) => {
-            console.log('notes = ', notes)
-            return notes
-        })
+    return asyncStorageService.query(NOTE_KEY)
+    .then((notes) => {
+        console.log('notes = ', notes)
+        return notes
+    })
 }
+
+function get(noteId) {
+    return storageService.get(NOTE_KEY, noteId)
+}
+
+
+
+
+
+
+
+    function _createNotes() {
+    
+        let notes = storageService.loadFromStorage(NOTE_KEY)
+    
+        if (!notes || !notes.length) {
+            notes = [
+                {
+                    id: "n101",
+                    type: "note-txt",
+                    isPinned: true,
+                    info: {
+                        txt: "Fullstack Me Baby!"
+                    }
+                },
+                {
+                    id: "n103",
+                    type: "note-todos",
+                    info: {
+                        lable: "Get my stuff together", todos: [
+                            { txt: "Driving liscence", doneAt: null },
+                            { txt: "Coding power", doneAt: 187111111 }]
+                    }
+                },
+                {
+                    id: "n102",
+                    type: "note-img",
+                    info: {
+                        url: "../../assets/img/fiat.jpg",
+                        title: "Bobi and Me"
+                    },
+                    style: {
+                        backgroundColor: "#00d"
+                    }
+                }
+            ]
+        }
+    
+        storageService.saveToStorage(NOTE_KEY, notes)
+    }
