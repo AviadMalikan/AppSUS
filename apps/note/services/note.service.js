1bf7cc537b71298b99a85af49291886e71cfcf9f
Lihi Ben Shimol <lihibs26@gmail.com>
2022-12-28 10:47:21 +0200

    logo

--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -1,1 +1,32 @@
 console.log('Hi')
+
+const notes = [
+    {
+        id: "n101",
+        type: "note-txt",
+        isPinned: true,
+        info: {
+            txt: "Fullstack Me Baby!"
+        }
+    },
+    {
+        id: "n103",
+        type: "note-todos",
+        info: {
+            label: "Get my stuff together", todos: [
+                { txt: "Driving liscence", doneAt: null },
+                { txt: "Coding power", doneAt: 187111111 }]
+        }
+    },
+    {
+        id: "n102",
+        type: "note-img",
+        info: {
+            url: "http://some-img/me",
+            title: "Bobi and Me"
+        },
+        style: {
+            backgroundColor: "#00d"
+        }
+    }
+]
--- apps/note/views/note-index.jsx
+++ apps/note/views/note-index.jsx
@@ -3,4 +3,5 @@ export function NoteIndex() {
 
     return <div>note app</div>
 
+    
 }
