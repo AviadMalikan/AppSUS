9b4d54fa1780c1c16467429233d7bd0dfe34af6a
AviadMalikan <aviad879@gmail.com>
2022-12-28 13:14:33 +0200

    add hamburger nav

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -1,18 +1,25 @@
 const { Link, NavLink } = ReactRouterDOM
+const { useState } = React
 
 export function AppHeader() {
+    const [isNavOpen, setIsNavOpen] = useState(true)
+
+    function closeOrOpenNav() {
+        setIsNavOpen(!isNavOpen)
+    }
 
     return <header className="app-header main-layout full">
-  
+
         <Link to="/">
             <h3>LOGO this is logo!</h3>
         </Link>
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/note">Note</NavLink>
-        </nav>
-  
-    </header>
+        <button className="fa fa-hamburger" onClick={closeOrOpenNav}></button>
+        {isNavOpen && <nav className="nav-menu" onClick={closeOrOpenNav}>
+            <NavLink to="/" className="fa fa-home"></NavLink>
+            <NavLink to="/about" className="fa fa-about"></NavLink>
+            <NavLink to="/mail" className="fa fa-mail "></NavLink>
+            <NavLink to="/note" className="fa fa-notes"></NavLink>
+        </nav>}
+
+    </header >
 }
