9d7ef6f866d2622459ce3f08e015cdc0155e3761
Lihi Ben Shimol <lihibs26@gmail.com>
2022-12-29 20:57:58 +0200

    home page

--- views/home.jsx
+++ views/home.jsx
@@ -1,7 +1,18 @@
+const { Link, NavLink } = ReactRouterDOM
+
 
 export function Home() {
 
     return <section className="home">
-        <h1>Welcome to home page!</h1>
+
+        <div className="main-logo">
+            <img src="./assets/img/app-logo/main-logo.png" alt="logo" className="main-logo-img" />
+        </div>
+
+        <div className="apps">
+            <NavLink className="app-link" to="/about"> <img src="./assets/img/about.png" alt="about" /></NavLink>
+            <NavLink className="app-link" to="/mail"> <img src="./assets/img/gmail.png" alt="mail" /></NavLink>
+            <NavLink className="app-link" to="/note"> <img src="./assets/img/keep.png" alt="keep" /></NavLink>
+        </div>
     </section>
 }
