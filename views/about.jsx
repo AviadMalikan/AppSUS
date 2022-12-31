a8de456e90a09a5ea9dfa99b4edd82d89d512fd4
AviadMalikan <aviad879@gmail.com>
2022-12-31 20:08:30 +0200

    update css on about section

--- views/about.jsx
+++ views/about.jsx
@@ -1,17 +1,42 @@
 
+const { Fragment } = React
+
 export function About() {
-    return <section className="about">
-        <h1>About Us</h1>
-        <h3 className="subtitle"> We are proud to present you <img className="about-logo" src="./assets/img/app-logo/main-logo.png" alt="" /></h3>
-        <p> An implementation to google's famous apps - Keep and Gmail. <br /> This project was built in joint work on Github collaboration as part of Coding Academy's Fullstack bootcamp, using ReactJS within 96 hours. </p>
-        <img className="team-bg" src="./img/team-bg.png" alt="" />
+    return <section className="main-about" >
+
+
+
+        <article className="about-app">
+
+            <h3 > We are proud to present you - AppSUS </h3>
+            <p> An implementation to google's famous apps - Keep and Gmail. <br /> This project was built in joint work on Github collaboration as part of Coding Academy's Fullstack bootcamp, using ReactJS within 96 hours. </p>
+        </article>
+
+
+
+
+        <div className="imgs">
+            <div className="gray-circle">
+
+                <img src="./img/logo-empty-gray.png" alt="circle-gray" />
+            </div>
+            <div className="yellow-circle">
+
+                <img src="./img/logo-empty-yellow.png" alt="circle-yellow" />
+            </div>
+        </div>
 
         <div className="team">
-            <article className="team-aviad">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque explicabo dolorum illo aliquam eum expedita provident voluptatum quia et reprehenderit.</article>
-            <hr />
-            <article className="team-lihi">I'm Lihi Ben Shimol, 24 years old from Tel-Aviv. <br />
-                At this project I was responsible for creating Appsus-keep - an implementation to Google Keep, and iBooks which allow you to build your own book-shop.</article>
+            <p>
+
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque explicabo dolorum illo aliquam eum expedita provident voluptatum quia et reprehenderit.
+            </p>
+            <p>
+                At this project I was responsible for creating Appsus-keep - an implementation to Google Keep, and iBooks which allow you to build your own book-shop.
+            </p>
         </div>
-        <p className="about-aviad"></p>
+        <small>
+            coffeeRights Lihi & Aviad
+        </small>
     </section>
 }
